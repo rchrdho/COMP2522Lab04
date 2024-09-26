9a1555d8b9bde03d337978f3d73ab8468e766296
Bryson Lindy <bslindy@gmail.com>
2024-09-26 11:48:53 -0700

    Created functional interfaces Printable and Reversible with comments

--- src/code/ca/bcit/comp2522/lab04/Reversible.java
+++ src/code/ca/bcit/comp2522/lab04/Reversible.java
@@ -1,6 +1,13 @@
 package ca.bcit.comp2522.lab04;
 
-public interface Reversible
+/**
+ * The Reversible interface is a specialized version of the Printable interface.
+ * It prints the details of Printable in reverse.
+ */
+public interface Reversible extends Printable
 {
-
+    /**
+     * Reverses the order of the details printed by Printable interface method display()
+     */
+    void backward();
 }
