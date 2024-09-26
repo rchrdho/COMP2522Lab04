07d6c23cd077ca4ede1b3dd63a305dd49430e408
HoRichard <richardho16@hotmail.com>
2024-09-26 11:56:08 -0700

    Made validators for Book class

--- src/code/ca/bcit/comp2522/lab04/Book.java
+++ src/code/ca/bcit/comp2522/lab04/Book.java
@@ -1,12 +1,16 @@
 package ca.bcit.comp2522.lab04;
 
 /**
- *
+ * @TODO validators for Data members
  */
 public class Book implements Comparable<Book>,
                              Printable,
                              Reversible
 {
+    private static final int MAX_TITLE_CHARS = 100;
+    private static final int CURRENT_YEAR    = 2024;
+    private static final int MIN_YEAR        = 1;
+
     private final String bookTitle;
     private final int    yearPublished;
     private final Author author;
@@ -15,18 +19,52 @@ public class Book implements Comparable<Book>,
                 final int    yearPublished,
                 final Author author)
     {
-        this.bookTitle = title;
-        this.yearPublished = yearPublished;
-        this.author = author;
+        validateTitle(title);
+        validateAuthor(author);
+
+        this.bookTitle      = title;
+        this.yearPublished  = yearPublished;
+        this.author         = author;
     }
 
-    private void isValidTitle(final String title)
+    private void validateTitle(final String title)
     {
+        if (title == null || title.isBlank() || title.length() >= MAX_TITLE_CHARS)
+        {
+            throw new IllegalArgumentException("Invalid title entry");
+        }
+    }
 
+    private void validateYearPublished(final int yearPublished)
+    {
+        if (yearPublished < MIN_YEAR || yearPublished > CURRENT_YEAR)
+        {
+            throw new IllegalArgumentException("Invalid year, outside of range");
+        }
+    }
+
+    private void validateAuthor(final Author author)
+    {
+        if (author == null)
+        {
+            throw new IllegalArgumentException("Invalid author entry");
+        }
     }
 
     @Override
     public int compareTo(Book o) {
         return 0;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public Author getAuthor() {
+        return author;
     }
 }
