048f5e84909ca29eb1a246238de6af535eda9e6e
Bryson Lindy <bslindy@gmail.com>
2024-09-29 18:59:37 -0700

    formatting fixes

--- src/code/ca/bcit/comp2522/lab04/Main.java
+++ src/code/ca/bcit/comp2522/lab04/Main.java
@@ -208,9 +208,13 @@ public class Main
             if (books[i].compareTo(books[i + 1]) > 0)
             {
                 System.out.println(books[i].getBookTitle() + " is older than " + books[i + 1].getBookTitle());
-            } else if (books[i].compareTo(books[i + 1]) < 0) {
+            }
+            else if (books[i].compareTo(books[i + 1]) < 0)
+            {
                 System.out.println(books[i].getBookTitle() + " is the same age as " + books[i + 1].getBookTitle());
-            } else {
+            }
+            else
+            {
                 System.out.println(books[i].getBookTitle() + " is younger than " + books[i + 1].getBookTitle());
             }
         }
